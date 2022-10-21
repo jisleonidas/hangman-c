c1775ba6e3412da552c134546e3ab7b9d7771495
Johann Sylvester <johannsylvester04@gmail.com>
2022-10-21 20:22:09 +0530

    Display obfuscated word without the consonants.
    The program is in a very rudimentary state. A function
    obfuscate_word has been added to main.c to replace the consonants
    in the word (which is used for playing hangman) with hyphens before
    displaying the word in that form with hyphens and vowels. The
    player will be expected to guess the missing letters.
    
    As of now, the program only does the obfuscation for one word,
    that is: 'hangman'.

--- main.c
+++ main.c
@@ -1,6 +1,50 @@
 #include <stdio.h>
 
+#define MAX_WORD_LENGTH 15
+
+#define TRUE 1
+#define FALSE 0
+
+void obfuscate_word(char *word);
+int check_vowel(char ch);
+
 int main()
 {
-	printf("Welcome to a game of HANGMAN!");
+
+	char word[MAX_WORD_LENGTH] = "hangman";
+
+	obfuscate_word(word);
+
+	printf("Welcome to a game of HANGMAN!\n");
+
+	printf("Word to be guessed: %s\n", word);
+
+	return 0;
+}
+
+void obfuscate_word(char *word)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i < 15; i++) {
+		ch = *word;
+		if (ch != '\0')
+			if (!check_vowel(ch))
+				*word = '-';
+		word++;
+	}
+}
+
+int check_vowel(char ch)
+{
+	int i;
+	char vowels[5] = "aeiou";
+
+	for (i = 0; i < 5; i++) {
+		if (ch == vowels[i])
+			return TRUE;
+		else
+			return FALSE;
+	}
 }
