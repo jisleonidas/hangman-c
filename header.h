ca4d9bdcde3434dd1f6c9bd701dab1b63a6bfb11
Johann Sylvester <johannsylvester04@gmail.com>
2022-10-22 13:11:14 +0530

    Added guess_handler for program.

---
+++ header.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_WORD_LENGTH 15
+
+#define TRUE 1
+#define FALSE 0
+
+int skipgarb()
+{
+        if (getchar() == '\n') {
+                return TRUE;
+        }
+        else {
+                while (TRUE) {
+                }
+        }
+        return FALSE;
+}
+
+int is_vowel(char ch)
+{
+	int i;
+	char vowels[5] = "aeiou";
+
+	for (i = 0; i < 5; i++) {
+		if (ch == vowels[i])
+			return TRUE;
+		else
+			return FALSE;
+	}
+}
+
+int is_alpha(char ch)
+{
+        int i;
+        char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++) {
+		if (ch == alphabet[i])
+			return TRUE;
+		else
+			return FALSE;
+	}
+}
